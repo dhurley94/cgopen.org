60c5a809998a5a7d8d1781a51ec0c28399438cc0
dhurley94 <drhurley94@gmail.com>
2017-11-03 10:42:15 -0400

    added route to carousel on index for newsletter

--- routes/index.js
+++ routes/index.js
@@ -44,4 +44,6 @@ exports = module.exports = function (app) {
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
 
+	app.get('/newsletter', middleware.requireUser, routes.views.newsletter);
+
 };
